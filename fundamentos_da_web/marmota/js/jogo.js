aa292f261584fdbca2451efba5b4d95b43ba6672
Thiago Mafort <thmafort@gmail.com>
2023-06-02 14:09:58 -0300

    adiciona jogo em fundamentos da web

---
+++ fundamentos_da_web/marmota/js/jogo.js
@@ -0,0 +1,23 @@
+const tela = document.querySelector(".score");
+const divs = document.querySelectorAll(".hole");
+
+divs.forEach((div) => {
+  div.addEventListener("click", function () {
+    if (div.classList.contains("up")) {
+      let pontos = parseInt(tela.textContent);
+      pontos++;
+      tela.textContent = pontos;
+      div.classList.remove("up");
+    }
+  });
+});
+
+setInterval(function () {
+  const numdiv = Math.trunc(Math.random() * 6) + 1;
+  const div = document.querySelector(`.hole${numdiv}`);
+  div.classList.add("up");
+
+  setTimeout(function () {
+    div.classList.remove("up");
+  }, 1000);
+}, 1000);
